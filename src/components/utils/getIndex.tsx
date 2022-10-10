d81c059ec4bcf262fd23911bcc02f8937617fa58
Stefan Marbun <stefanmarbun12@gmail.com>
2022-10-10 12:37:31 +1100

    fix: getindex

--- src/components/utils/getIndex.tsx
+++ src/components/utils/getIndex.tsx
@@ -1,9 +1,11 @@
+import get from 'lodash.get';
+
 export const getIndex = (name: string): number | null => {
   const components = name.split('.');
   const len = components.length;
   if (len > 1) {
     // assuming this field isn't part of a nested object
-    return parseInt(components[len - 2].split('[')[1]);
+    return parseInt(get(components[len - 2].split('['), '[1]'));
   }
   return null;
 };
